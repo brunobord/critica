8bc4331c84a374f2e84ef4a173b903bdce53ecf0
Gilles Fabio <gilles.fabio@gmail.com>
2008-09-08 14:27:07 +0200

    Added layout, home and category templates (with sample content from designer), and removed obsolete archive templates.

--- apps/journal/views.py
+++ apps/journal/views.py
@@ -3,15 +3,24 @@ Views for ``critica.apps.journal``.
 
 """
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 from critica.apps.journal.models import Page
 
 
 def home(request):
-    page = Page.complete.filter(category__pk=1)[0]
-    return render_to_response('journal/home.html', {'page': page})
+    #page = Page.complete.filter(category__pk=1)[0]
+    return render_to_response(
+        'journal/home.html', 
+        {}, 
+        context_instance=RequestContext(request)
+    )
     
 def category(request, category):
-    return render_to_response('journal/category.html', {})
+    return render_to_response(
+        'journal/category.html', 
+        {}, 
+        context_instance=RequestContext(request)
+    )
     
 def archives(request):
     return render_to_response('journal/archives.html', {})
