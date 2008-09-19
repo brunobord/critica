511cadafb0f56471439ef8438f944271b2cecb10
Gilles Fabio <gilles.fabio@gmail.com>
2008-09-19 12:18:25 +0200

    Added links into advanced admin indx page, cleaned XHTML/CSS and registered Site/User/Group models into basic and advanced sites.

--- apps/admin/sites.py
+++ apps/admin/sites.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.contrib.admin.sites import AdminSite
+from django import http, template
 
 
 # Basic admin
@@ -18,3 +19,22 @@ class AdvancedAdminSite(AdminSite):
 # ------------------------------------------------------------------------------
 basic_site = BasicAdminSite()
 advanced_site = AdvancedAdminSite()
+
+
+# Default registers
+# ------------------------------------------------------------------------------
+from django.contrib.auth.models import User
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
+
+basic_site.register(User, UserAdmin)
+basic_site.register(Group, GroupAdmin)
+
+advanced_site.register(User, UserAdmin)
+advanced_site.register(Group, GroupAdmin)
+
+from django.contrib.sites.models import Site
+from django.contrib.sites.admin import SiteAdmin
+
+basic_site.register(Site, SiteAdmin)
+advanced_site.register(Site, SiteAdmin)
