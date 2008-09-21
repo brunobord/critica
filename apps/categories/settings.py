4877939e69c6b8af18166c37232453b392a4a67d
Gilles Fabio <gilles.fabio@gmail.com>
2008-09-21 10:39:27 +0200

    Added apps settings files (categories and illustrations).

---
+++ apps/categories/settings.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+# Category image upload path
+IMAGE_UPLOAD_PATH = 'upload/categories/'
---
+++ apps/illustrations/settings.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+# Illustration image upload path
+IMAGE_UPLOAD_PATH = 'upload/%Y/%m/%d'
+
+
