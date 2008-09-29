2608c7985db4a08c56e32dbbbeb9633b68b1bd47
Gilles Fabio <gilles.fabio@gmail.com>
2008-09-29 17:04:45 +0200

    Added admin index permission checking.

--- apps/admin/sites.py
+++ apps/admin/sites.py
@@ -1,16 +1,44 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 from django.contrib.admin.sites import AdminSite
 from django import http, template
 from django.shortcuts import render_to_response
 from django.template import RequestContext
-from datetime import datetime
+from django.utils.translation import ugettext_lazy, ugettext as _
+from django.views.decorators.cache import never_cache
 
 
 # Basic admin
 # ------------------------------------------------------------------------------
 class BasicAdminSite(AdminSite):
-    index_template = 'basic_admin/index.html'
+    """
+    An AdminSite object encapsulates an instance of the Django admin application, ready
+    to be hooked in to your URLConf. Models are registered with the AdminSite using the
+    register() method, and the root() method can then be used as a Django view function
+    that presents a full admin interface for the collection of registered models.
     
+    BasicAdminSite is a basic Django admin with lightweight customizations.
+    
+    """
+    index_template = 'basic_admin/index.html'
+
+
+    def index(self, request, extra_context=None):
+        """
+        Displays the main admin index page, which lists all of the installed
+        apps that have been registered in this site.
+        
+        """
+        context = {
+            'title': _('Site administration'),
+            'root_path': self.root_path,
+        }
+        context.update(extra_context or {})
+        return render_to_response(self.index_template or 'admin/index.html', context,
+            context_instance=template.RequestContext(request)
+        )
+    index = never_cache(index)
+
     def dashboard_index(self, request):
         """
         Basic admin dashboard view.
@@ -29,6 +57,15 @@ class BasicAdminSite(AdminSite):
 # Advanced admin
 # ------------------------------------------------------------------------------    
 class AdvancedAdminSite(AdminSite):
+    """
+    An AdminSite object encapsulates an instance of the Django admin application, ready
+    to be hooked in to your URLConf. Models are registered with the AdminSite using the
+    register() method, and the root() method can then be used as a Django view function
+    that presents a full admin interface for the collection of registered models.
+    
+    AdvancedAdminSite is an advanced Django admin with heavy customizations.
+    
+    """
     index_template = 'advanced_admin/index.html'
 
 
