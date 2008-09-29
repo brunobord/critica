1f93b82c157962ee30317c2c58ad7b4543434ddd
Gilles Fabio <gilles.fabio@gmail.com>
2008-09-29 12:38:17 +0200

    Removed CategoryPositionInline from Issue model.

--- apps/issues/admin.py
+++ apps/issues/admin.py
@@ -1,26 +1,16 @@
 # -*- coding: utf-8 -*-
 """ 
-Administration interface options for ``critica.apps.issues`` models. 
+Administration interface options of ``critica.apps.issues`` models. 
 
 """
 from django.contrib import admin
 from critica.apps.admin.sites import basic_site, advanced_site
 from critica.apps.issues.models import Issue
-from critica.apps.positions.models import CategoryPosition
-
-
-class CategoryPositionInline(admin.TabularInline):
-    """
-    Category position inline for ``Issue`` model.
-    
-    """
-    model = CategoryPosition
-    max_num = 13
     
     
 class IssueAdmin(admin.ModelAdmin):
     """
-    Administration interface for ``Issue`` model.
+    Administration interface options of ``Issue`` model.
     
     """
     list_display = ('number', 'publication_date', 'status')
@@ -29,8 +19,6 @@ class IssueAdmin(admin.ModelAdmin):
     ordering = ('-publication_date',)
     radio_fields = {'status': admin.VERTICAL}
     date_hierarchy = 'publication_date'
-    inlines = [CategoryPositionInline]
-
 
 admin.site.register(Issue, IssueAdmin)
 basic_site.register(Issue, IssueAdmin)
