abafeb72ce6fcff31b2a8c2f3d4b00f1a349ad8e
Gilles Fabio <gilles.fabio@gmail.com>
2008-10-01 10:15:51 +0200

    Added articles/settings.py.

---
+++ apps/articles/settings.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Settings of ``critica.apps.articles`` application.
+
+"""
+
+# Excluded categories
+# ------------------------------------------------------------------------------
+EXCLUDED_CATEGORIES = [
+    'epicurien',
+    'voyages',
+    'regions',
+    'coup-de-gueule',
+]
+
