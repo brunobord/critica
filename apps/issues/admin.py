827735db64b0618f43e6fd572c71bdc3c3d83ca2
Gilles Fabio <gilles.fabio@gmail.com>
2008-10-05 09:24:45 +0200

    Added auto-creating positions for issue feature and updated users fixtures (initial / sample).

--- apps/issues/admin.py
+++ apps/issues/admin.py
@@ -23,37 +23,29 @@ class IssueAdmin(admin.ModelAdmin):
         obj.save()
         if change == False:
             from django.conf import settings
-            from critica.apps.positions import settings as positions_settings
-            from critica.apps.categories.models import Category
-            """
             if 'critica.apps.categories' in settings.INSTALLED_APPS:
-                from critica.apps.positions.models import CategoryPosition
-                for slug in positions_settings.CATEGORY_DEFAULT_ORDER:
-                    # defining default position if it does exist
-                    if slug in positions_settings.CATEGORY_DEFAULT_POSITION:
-                        default_position = positions_settings.CATEGORY_DEFAULT_POSITION[slug]
-                    else:
-                        default_position = None
-                    # the category must exist
-                    category = Category.objects.get(slug=slug)
-                    # create the category
-                    position = CategoryPosition(issue=obj, category=category, position=default_position)
-                    position.save()
-            if 'critica.apps.notes' in settings.INSTALLED_APPS: 
-                from critica.apps.notes.models import NoteType
-                from critica.apps.positions.models import NoteTypePosition
-                categories = Category.objects.exclude(slug__in=positions_settings.EXCLUDED_CATEGORIES)
-                for category in categories:
-                    for slug in positions_settings.NOTE_TYPE_DEFAULT_ORDER:
-                        if slug in positions_settings.NOTE_TYPE_DEFAULT_POSITION:
-                            default_position = positions_settings.NOTE_TYPE_DEFAULT_POSITION[slug]
-                        else:
-                            default_position = None
-                        note_type = NoteType.objects.get(slug=slug)
-                        position = NoteTypePosition(issue=obj, category=category, type=note_type, position=default_position)
-                        position.save() 
-            """
+                from critica.apps.positions.models import DefaultCategoryPosition
+                from critica.apps.positions.models import IssueCategoryPosition
+                default_positions = DefaultCategoryPosition.objects.all()
+                for default_position in default_positions:
+                    issue_position = IssueCategoryPosition(
+                        issue=obj, 
+                        category=default_position.category, 
+                        position=default_position.position)
+                    issue_position.save()
+            if 'critica.apps.notes' in settings.INSTALLED_APPS:
+                from critica.apps.positions.models import DefaultNotePosition
+                from critica.apps.positions.models import IssueNotePosition
+                default_positions = DefaultNotePosition.objects.all()
+                for default_position in default_positions:
+                    issue_position = IssueNotePosition(
+                        issue=obj,
+                        category=default_position.category,
+                        type=default_position.type,
+                        position=default_position.position)
+                    issue_position.save()
             if 'critica.apps.quotas' in settings.INSTALLED_APPS:
+                from critica.apps.categories.models import Category
                 from critica.apps.quotas.models import DefaultCategoryQuota
                 from critica.apps.quotas.models import CategoryQuota
                 default_quotas = DefaultCategoryQuota.objects.all()
