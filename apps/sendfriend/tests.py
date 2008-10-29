e222d6a5a7df32fffd84ca6e6d6000054fca4079
Gilles Fabio <gilles.fabio@gmail.com>
2008-10-29 09:06:02 +0100

    Added tests to sendfriend application.

--- apps/sendfriend/tests.py
+++ apps/sendfriend/tests.py
@@ -3,7 +3,6 @@
 Tests of ``critica.apps.sendfriend`` application.
 
 """
-from django.conf import settings
 from django.test import TestCase
 from django.core import mail
 from django.core.urlresolvers import reverse
@@ -13,12 +12,15 @@ from django.utils.translation import ugettext as _
 
 class SendFriendTestCase(TestCase):
     """
-    Send friend test case.
+    SendFriend application test case.
     
     """
+    fixtures = ['sample_data']
+
+
     def _test_sendfriend(self, fields):
         """
-        Private: returns the result of a POST query using the send friend form.
+        Private: returns the result of a POST query using the sendfriend form."
         
         """
         return self.client.post(reverse('sendfriend'), fields)
@@ -26,7 +28,7 @@ class SendFriendTestCase(TestCase):
 
     def test_sendfriend_form_get(self):
         """
-        What do we do with a 'get' request.
+        GET request on sendfriend form.
         
         """
         response = self.client.get(reverse('sendfriend'))
@@ -35,37 +37,49 @@ class SendFriendTestCase(TestCase):
 
     def test_sendfriend_form_ok(self):
         """
-        Send friend form processing.
+        SendFriend form processing.
         
         """
         fields = {
+            'sender': 'Gilles Fabio',
             'to_email': 'gfabio@interfaceip.com',
         }
+        
         response = self._test_sendfriend(fields)
-        self.assertRedirects(response, reverse('sendfriend_ok'), status_code=302, target_status_code=200)
+        self.assertRedirects(response, reverse('sendfriend_thanks'), status_code=302, target_status_code=200)
         self.assertEquals(len(mail.outbox), 1) # 1 mail sent
         self.assertEquals(mail.outbox[0].to, [fields['to_email']])
         self.assertEquals(mail.outbox[0].from_email, settings.DEFAULT_FROM_EMAIL)
-        # testing body
         self.assertTrue(mail.outbox[0].body.startswith('<p>Bonjour,'))
 
 
     def test_sendfriend_form_empty(self):
         """
-        Empty form, error.
+        Empty sendfriend form.
         
         """
-        response = self._test_sendfriend({'to_email': ''})
-        self.assertFormError(response, "form", 'to_email', _('This field is required.'))
+        fields = {
+            'sender': '',
+            'to_email': '',
+        }
+        
+        response = self._test_sendfriend(fields)
+        self.assertFormError(response, 'form', 'sender', _('This field is required.'))
+        self.assertFormError(response, 'form', 'to_email', _('This field is required.'))
 
 
     def test_sendfriend_wrong_email(self):
         """
-        Empty form, error.
+        SendFriend form with a wrong email.
         
         """
-        response = self._test_sendfriend({'to_email': 'ddd'})
-        self.assertFormError(response, "form", 'to_email', _('Enter a valid e-mail address.'))
+        fields = {
+            'sender': 'Gilles Fabio',
+            'to_email': 'azerty',
+        }
+        
+        response = self._test_sendfriend(fields)
+        self.assertFormError(response, 'form', 'to_email', _('Enter a valid e-mail address.'))
 
 
 
