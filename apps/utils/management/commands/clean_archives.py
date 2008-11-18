870dc33f0523f47fda067ed4a9eb571dd8a3b37d
Gilles Fabio <gilles.fabio@gmail.com>
2008-11-18 16:38:03 +0100

    Improved clean_archives command.

--- apps/utils/management/commands/clean_archives.py
+++ apps/utils/management/commands/clean_archives.py
@@ -2,6 +2,60 @@
 from django.core.management.base import NoArgsCommand
 from django.core.management.base import CommandError
 
+CATEGORY_MAP = [
+    (1, 4010),
+    (2, 4011),
+    (3, 4002),
+    (4, 4003),
+    (5, 4004),
+    (6, 4005),
+    (7, 4006),
+    (8, 4007),
+    (9, 4008),
+]
+
+NOTE_TYPE_MAP = [
+    (1, 4008),
+    (2, 4004),
+    (3, 4009),
+    (4, 4003),
+    (5, 4007),
+    (6, 4001),
+    (7, 4006),
+    (8, 4010),
+    (9, 4005),
+    (10, 4002),
+]
+
+REGION_MAP = [
+    (1, 4018),
+    (2, 4004),
+    (3, 4001),
+    (4, 4005),
+    (5, 4002),
+    (6, 4006),
+    (7, 4003),
+    (8, 4007),
+    (9, 4026),
+    (10, 4011),
+    (11, 4009),
+    (12, 4012),
+    (13, 4010),
+    (14, 4013),
+    (15, 4014),
+    (16, 4015),
+    (17, 4016),
+    (18, 4017),
+    (19, 4008),
+    (20, 4025),
+    (21, 4019),
+    (22, 4020),
+    (23, 4021),
+    (24, 4022),
+    (25, 4023),
+    (26, 4024),
+]
+
 
 class Command(NoArgsCommand):
     """
@@ -13,6 +67,9 @@ class Command(NoArgsCommand):
         Handler.
         
         """
+        print "Loading fixtures..."
+        self._initialize()
+        
         print "\nANGER / ARTICLES COUP DE GUEULE"
         self._draw_separator()
         self.clean_anger()
@@ -40,6 +97,8 @@ class Command(NoArgsCommand):
         print "\nVOYAGES / ARTICLES VOYAGES"
         self._draw_separator()
         self.clean_voyages()
+        
+        print "\n"
 
     
     def clean_anger(self):
@@ -47,8 +106,27 @@ class Command(NoArgsCommand):
         
     
     def clean_articles(self):
-        print "Nothing yet..."
+        """
+        Clean articles.
         
+        """
+        from critica.apps.categories.models import Category
+        from critica.apps.articles.models import Article
+        
+        # Update categories
+        # ----------------------------------------------------------------------
+        for category in CATEGORY_MAP:
+            articles = Article.objects.filter(category__id=category[1])
+            for article in articles:
+                new_category = Category.objects.get(id=category[0])
+                article.category = new_category
+                article.save()       
+        print "Update categories... OK."
+        
+        # Set featured boolean
+        # ----------------------------------------------------------------------
+        self._set_featured(Article)
+
 
     def clean_epicurien(self):
         """
@@ -67,7 +145,7 @@ class Command(NoArgsCommand):
                 article.delete()
         except:
             pass
-        print "Delete empty articles... OK.\n"
+        print "Delete empty articles... OK."
         
         # Update types
         #
@@ -85,19 +163,19 @@ class Command(NoArgsCommand):
         for article in articles_bar:
             article.type = type_cotebar
             article.save()
-        print "Update 'Cote bar' type... OK.\n"
+        print "Update 'Cote bar' type... OK."
         
         articles_gourmets = EpicurienArticle.objects.filter(category__id=4013)
         for article in articles_gourmets:
             article.type = type_cotegourmets
             article.save()
-        print "Update 'Cote Gourmets' type... OK.\n"
+        print "Update 'Cote Gourmets' type... OK."
             
         articles_fumeurs = EpicurienArticle.objects.filter(category__id=4012)
         for article in articles_fumeurs:
             article.type = type_cotefumeurs
             article.save()
-        print "Update 'Cote fumeurs' type... OK.\n"
+        print "Update 'Cote fumeurs' type... OK."
         
         # Regroup issues
         # ----------------------------------------------------------------------
@@ -125,16 +203,56 @@ class Command(NoArgsCommand):
         Clean notes.
         
         """
-        print "Nothing yet..."
+        from critica.apps.categories.models import Category
+        from critica.apps.notes.models import NoteType
+        from critica.apps.notes.models import Note
+        
+        # Update categories
+        # ----------------------------------------------------------------------
+        for category in CATEGORY_MAP:
+            notes = Note.objects.filter(category__id=category[1])
+            for note in notes:
+                new_category = Category.objects.get(id=category[0])
+                note.category = new_category
+                note.save()       
+        print "Update categories... OK."
 
+        # Update types
+        # ----------------------------------------------------------------------
+        for note_type in NOTE_TYPE_MAP:
+            notes = Note.objects.filter(type__id=note_type[1])
+            for note in notes:
+                new_type = NoteType.objects.get(id=note_type[0])
+                note.type = new_type
+                note.save()
+        print "Update types... OK."
+        
+        # Set featured boolean
+        # ----------------------------------------------------------------------
+        self._set_featured(Note)
+        
 
     def clean_region_notes(self):
         """
         Clean region notes.
         
         """
-        print "Nothing yet..."
+        from critica.apps.regions.models import RegionNote
+        from critica.apps.regions.models import Region
 
+        # Update regions
+        # Category is automatically updated at saving
+        # ----------------------------------------------------------------------
+        for region in REGION_MAP:
+            notes = RegionNote.objects.filter(region__id=region[1])
+            for note in notes:
+                new_region = Region.objects.get(id=region[0])
+                note.region = new_region
+                note.is_ready_to_publish = True
+                note.is_reserved = False
+                note.save()
+        print "Update regions... OK."
+        
 
     def clean_voyages(self):
         """
@@ -144,7 +262,7 @@ class Command(NoArgsCommand):
         from critica.apps.voyages.models import VoyagesArticle    
         self._regroup_issues(VoyagesArticle)
 
-     
+    
     def _draw_separator(self):
         """
         Draw article type separator.
@@ -152,7 +270,16 @@ class Command(NoArgsCommand):
         """
         print 80 * '-' + '\n'
 
+    
+    def _initialize(self):
+        """
+        Initialization.
         
+        """
+        import os
+        load_fixtures = os.system('./manage.py loaddata data/archives/*')
+        
+    
     def _get_slugs(self, model):
         """
         Returns a list of model slugs.
@@ -184,16 +311,24 @@ class Command(NoArgsCommand):
                 for issue in issues:
                     item.issues.add(issue)    
                     item.save()
-                display_issues = []
-                for i in item.issues.all():
-                    display_issues.append(int(i.number))
-                print "%s: %s" % (item.title, display_issues)
                 changed = True
-        print "\nRegroup issues... OK.\n"
+        print "Regroup issues... OK."
         items = model.objects.all()
         for item in items:
             if len(item.issues.all()) < 2:
                 item.delete()
-        print "Delete extra articles... OK.\n"
+        print "Delete extra articles... OK."
+       
+        
+    def _set_featured(self, model):
+        """
+        Sets 'is_featured' field to True if item's title contains the word 'exclusif'.
+        
+        """
+        items = model.objects.filter(title__icontains='exclusif')
+        for item in items:
+            item.is_featured = True
+            item.save()
+        print "Set featured... OK."
 
 
