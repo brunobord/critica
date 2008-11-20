30479324694937ac323797cb37dbb5d5481cd8cc
Gilles Fabio <gilles.fabio@gmail.com>
2008-11-20 11:11:44 +0100

    Updated some fields of newsletter subscriber model (NOT NULL => NULL).

--- apps/newsletter/models.py
+++ apps/newsletter/models.py
@@ -12,10 +12,10 @@ class Subscriber(models.Model):
     Newsletter subscriber
     
     """
-    first_name = models.CharField(_('first name'), max_length=255, blank=True)
-    last_name = models.CharField(_('last name'), max_length=255, blank=True)
-    email = models.EmailField(_('email'), max_length=255, unique=True) 
-    zip_code = models.CharField(_('zip code'), max_length=5, blank=True)
+    first_name        = models.CharField(_('first name'), max_length=255, null=True, blank=True)
+    last_name         = models.CharField(_('last name'), max_length=255, null=True, blank=True)
+    email             = models.EmailField(_('email'), max_length=255, unique=True) 
+    zip_code          = models.CharField(_('zip code'), max_length=5, null=True, blank=True)
     registration_date = models.DateField(_('registration date'), auto_now_add=True) 
 
     class Meta:
@@ -23,7 +23,7 @@ class Subscriber(models.Model):
         Model metadata. 
         
         """
-        verbose_name = _('subscriber')
+        verbose_name        = _('subscriber')
         verbose_name_plural = _('subscribers')
 
     def __unicode__(self):
